7c7a3c957cedd5e573c88d5e149553d34cc55f90
Martin <arkadiainteractiveok@gmail.com>
2024-09-24 20:17:08 -0300

    destruction by fragments

--- Fragments/Frags/Scrips/frag.gd
+++ Fragments/Frags/Scrips/frag.gd
@@ -4,14 +4,13 @@ var frag_scene
 var frag_instance
 
 func _ready() -> void:
-	frag_scene = load("res://Fragments/BasicFragment/Scenes/basic_fragment.tscn")
+	frag_scene = load("res://Fragments/LongFragment/Scenes/long_fragment.tscn")
 	$"..".connect("exploded", _on_exploded_signal)
 
 func Start() -> void:
 	frag_instance = frag_scene.instantiate()
 	frag_instance.position = global_position
 	apply_random_force(frag_instance)
-	#frag_instance.apply_impulse(Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)).normalized(), Vector3(0,0,0))
 	get_tree().current_scene.add_child(frag_instance)
 
 func _on_exploded_signal():
@@ -21,9 +20,10 @@ func apply_random_force(fragment: RigidBody3D):
 	var force_magnitude: float = randf_range(1, 50)
 	# Genera un Vector3 con componentes aleatorias entre -1 y 1
 	var random_direction = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1)).normalized()
-
+	var random_torque = Vector3(randf_range(-10, 10), randf_range(-10, 10), randf_range(-10, 10))
 	# Multiplica el vector normalizado por la magnitud de la fuerza deseada
 	var force = random_direction * force_magnitude
 
 	# Aplica la fuerza al fragmento
 	fragment.apply_impulse(force, Vector3.ZERO)
+	fragment.apply_torque(random_torque)
--- Fragments/Frags/Scrips/fragments.gd
+++ Fragments/Frags/Scrips/fragments.gd
@@ -2,15 +2,22 @@ extends Node3D
 
 signal exploded
 signal destroy
+var impact_counter = 0
+var critical_impacts
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	impact_counter += 1
+	if body.collision_layer == 2 :
+		Destroy()
+	elif impact_counter > critical_impacts:
+		Destroy()
+
+	print ("CANTIDAD DE IMPACTOS: ", impact_counter)
+
+func Destroy():
 	emit_signal("exploded")
 	emit_signal("destroy")
